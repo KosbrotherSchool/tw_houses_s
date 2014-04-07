444899e2bfbaa6d9f2261463dd16f29e6b24bf1d
KosbrotherSchool <kosbrotherschool@gmail.com>
2014-04-07 23:27:12 +0800

    change deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "tw_houses"
 set :rails_env, "production"
 
 set :branch, "master"
-set :repository,  "https://github.com/KosbrotherSchool/tw_houses_s"
+set :repository,  "git@github.com:KosbrotherSchool/tw_houses_s.git"
 set :scm, "git"
 set :user, "apps" # 一個伺服器上的帳戶用來放你的應用程式，不需要有sudo權限，但是需要有權限可以讀取Git repository拿到原始碼
 
